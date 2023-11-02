14d2c40d9a93be85fe86e7ca3bc72f4c7d030aa3
HEMANTH123SR <applicationcomputer257@gmail.com>
2023-11-02 14:43:49 +0530

    completed the basic app funcationality app is in working condition

--- todo-app/src/components/TodoItem.jsx
+++ todo-app/src/components/TodoItem.jsx
@@ -1,6 +1,17 @@
-function TodoItem({ todo }) {
-    
+import { useState } from "react";
+import { useTodo } from "../context/TodoContext";
 
+function TodoItem({ todo }) {
+    const [isTodoEditable,setIsTodoEditable]=useState(false)
+    const [todoMsg,setTodoMsg]=useState(todo.todo)
+    const {upDatedTodo,delTodo,toggleComplete}=useTodo();
+const editTodo=()=>{
+    upDatedTodo(todo.id,{...todo,todo:todoMsg})
+    setIsTodoEditable(false)
+}
+const toggleCompleted=()=>{
+toggleComplete(todo.id)
+}
     return (
         <div
             className={`flex border border-black/10 rounded-lg px-3 py-1.5 gap-x-3 shadow-sm shadow-white/50 duration-300  text-black ${
@@ -39,7 +50,7 @@ function TodoItem({ todo }) {
             {/* Delete Todo Button */}
             <button
                 className="inline-flex w-8 h-8 rounded-lg text-sm border border-black/10 justify-center items-center bg-gray-50 hover:bg-gray-100 shrink-0"
-                onClick={() => deleteTodo(todo.id)}
+                onClick={() => delTodo(todo.id)}
             >
                 ❌
             </button>
